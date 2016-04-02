949531844e285c9b34aaab7be341327a1d16900b
zpf <zhaopengfei@fanlitou.com>
2016-04-02 23:41:19 +0800

    fixed

--- kill.sh
+++ kill.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-
 kill(){
     process_name=$1
     ps -ef | grep $process_name | grep -v grep | grep -v kill.sh| grep -v vim | cut -c 9-15 | xargs kill -9
@@ -9,5 +8,5 @@ if [ $# -eq 0 ]
 then
     :
 else
-    killBz $1   
+    kill $1   
 fi
